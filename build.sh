6b5b739896c5db0c9b83019064812ec0ee4dad5b
InstigatorX <sloebric@gmail.com>
2013-12-03 09:09:33 -0800

    BUILD: Updated for GE 4.4

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@ set -e
 
 timestamp=$(date '+%s')
 
+export ARCH=arm
+export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.7/bin/arm-linux-androideabi-
 export LOCALVERSION="$timestamp"
 
 #export VARIANT_DEFCONFIG=ix_jf_defconfig; make ix_jf_defconfig
@@ -19,7 +21,7 @@ cd ../build_dir
 
 ../../packaging/mkbootimg.new --kernel ../arch/arm/boot/zImage --ramdisk ../ramdisk/boot.img-ramdisk.gz --cmdline "console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3" --base "0x80200000" --ramdisk_offset "0x82200000" --output boot.img
 
-zip -r ~/Google\ Drive/Kernels/TW4.3/iX-TW-4.3-MJ5-jf-Kernel-"$timestamp"$1.zip *
+zip -r ~/Google\ Drive/Kernels/TW4.3/iX-GE-4.4-jf-Kernel-"$timestamp"$1.zip *
 
 echo $timestamp
 
