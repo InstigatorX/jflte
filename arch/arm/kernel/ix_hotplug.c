732f87fe8f79c96734da4831852de12810ce41cb
InstigatorX <sloebric@gmail.com>
2014-03-21 14:41:37 -0700

    IX_HOTPLUG: Some further tweaks
    Signed-off-by: InstigatorX <sloebric@gmail.com>

--- arch/arm/kernel/ix_hotplug.c
+++ arch/arm/kernel/ix_hotplug.c
@@ -38,8 +38,9 @@
 #include <linux/mutex.h>
 #include <linux/platform_device.h>
 #include <linux/cpufreq.h>
-#include <linux/kernel_stat.h>
-#include <linux/tick.h>
+//#include <linux/kernel_stat.h>
+//#include <linux/tick.h>
+//#include <linux/rq_stats.h>
 
 #ifdef CONFIG_HAS_EARLYSUSPEND
 #include <linux/earlysuspend.h>
@@ -62,10 +63,10 @@ static struct delayed_work hotplug_decision_work;
 static struct work_struct suspend;
 static struct work_struct resume;
 
-static unsigned int enable_all_load = 700;
-static unsigned int enable_load[5] = {0, 170, 235, 300, 0};
-static unsigned int disable_load[5] = {0, 0, 70, 135, 200};
-static unsigned int sample_rate[5] = {0, 50, 50, 100, 200};
+static unsigned int enable_all_load = 800;
+static unsigned int enable_load[5] = {0, 100, 220, 320, 0};
+static unsigned int disable_load[5] = {0, 0, 30, 160, 240};
+static unsigned int sample_rate[5] = {0, 50, 150, 100, 50};
 static unsigned int load_enable;
 static unsigned int load_disable;
 static unsigned int sampling_rate;
@@ -74,14 +75,15 @@ static unsigned int available_cpus = 4;
 static unsigned int online_sample = 1;
 static unsigned int offline_sample = 1;
 static unsigned int online_sampling_periods = 3;
-static unsigned int offline_sampling_periods = 5;
+//static unsigned int offline_sampling_periods = 6;
+static unsigned int offline_sampling_periods[5] = {0, 0, 10, 5, 4};
 static unsigned int online_cpus;
-static unsigned int min_cpus_online = 2;
+static unsigned int min_cpus_online = 1;
 
 static void hotplug_online_single_work(void)
 {
 	cpu_up(online_cpus);
-	//pr_info("ix_hotplug: CPU%d up.\n", cpu);
+	pr_info("ix_hotplug: CPU%d up.\n", online_cpus);
 	return;
 }
 
@@ -92,7 +94,7 @@ static void hotplug_online_all_work(void)
 	for_each_possible_cpu(cpu) {
 		if (likely(!cpu_online(cpu))) {
 			cpu_up(cpu);
-			//pr_info("ix_hotplug: CPU%d up.\n", cpu);
+			pr_info("ix_hotplug: CPU%d up.\n", cpu);
 		}
 	}
 	return;
@@ -101,23 +103,25 @@ static void hotplug_online_all_work(void)
 static void hotplug_offline_work(void)
 {
 	cpu_down(online_cpus - 1);
-	//pr_info("ix_hotplug: CPU%d down.\n", idlest);
+	pr_info("ix_hotplug: CPU%d down.\n", (online_cpus - 1));
 	return;
 }
 
 static void __ref hotplug_decision_work_fn(struct work_struct *work)
 {
 	unsigned int avg_running, io_wait;
-
+	//unsigned int rq_depth;
+	
 	sched_get_nr_running_avg(&avg_running, &io_wait);
-
+	//rq_depth = rq_info.rq_avg;
+	
 	load_disable = disable_load[online_cpus];
 
 	if (avg_running <= load_disable && online_cpus > min_cpus_online) {
-		if (offline_sample >= offline_sampling_periods) {
+		if (offline_sample >= offline_sampling_periods[online_cpus]) {
 			hotplug_offline_work();
 			offline_sample = 1;
-			//pr_info("ix_hotplug[OFF]: %d AR: %d SR: %d IO: %d\n", load_disable, avg_running, sampling_rate, io_wait);
+			//pr_info("ix_hotplug: Threshold: %d Load: %d Sampling: %d RQ: %d\n", load_disable, avg_running, sampling_rate, rq_depth);
 		} else {
 			offline_sample++;
 		}
@@ -129,7 +133,7 @@ static void __ref hotplug_decision_work_fn(struct work_struct *work)
 
 		if (avg_running >= enable_all_load) {
 			hotplug_online_all_work();
-			//pr_info("ix_hotplug[ALL]: AR: %d SR: %d IO: %d\n", avg_running, sampling_rate, io_wait);
+			//pr_info("ix_hotplug: Threshold: %d Load: %d Sampling: %d RQ: %d\n", enable_all_load, avg_running, sampling_rate, rq_depth);
 			offline_sample = 1;
 			goto exit;
 		}				
@@ -140,7 +144,7 @@ static void __ref hotplug_decision_work_fn(struct work_struct *work)
 			if (online_sample >= online_sampling_periods) { 
 				hotplug_online_single_work();
 				online_sample = 1;
-				//pr_info("ix_hotplug[ON]: %d AR: %d SR: %d IO: %d\n", load_enable, avg_running, sampling_rate, io_wait);
+				//pr_info("ix_hotplug: Threshold: %d Load: %d Sampling: %d RQ: %d\n", load_enable, avg_running, sampling_rate, rq_depth);
 			} else {
 				online_sample++;
 			}
@@ -153,7 +157,9 @@ exit:
 	online_cpus = num_online_cpus();
 
 	sampling_rate = sample_rate[online_cpus] * load_multiplier;
-
+	
+	//pr_info("ix_hotplug: CPUs: %d Load: %d Sampling: %d RQ: %d\n", online_cpus, avg_running, sampling_rate, rq_depth);
+	
 	schedule_delayed_work_on(0, &hotplug_decision_work, msecs_to_jiffies(sampling_rate));
 }
 
@@ -172,7 +178,7 @@ static void __ref ix_hotplug_resume(struct work_struct *work)
 {
 	mutex_lock(&ix_hotplug_mutex);
 	load_multiplier = 1;
-	min_cpus_online = 2;
+	min_cpus_online = 1;
 	mutex_unlock(&ix_hotplug_mutex);
 
 	//pr_info("ix_hotplug: late resume handler\n");
